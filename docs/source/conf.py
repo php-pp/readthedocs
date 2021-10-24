67688771d6203195a1b0554bb6763c5cdb485c74
Steevan BARBOYON <steevan.barboyon@gmail.com>
2021-10-24 22:46:52 +0200

    Documentation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'PHP++'
+copyright = '2021, Steevan BARBOYON'
+author = 'Steevan BARBOYON'
 
 release = '0.1'
 version = '0.1.0'
